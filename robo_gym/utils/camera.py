5fb571042454c7fcec2441c1e1877c6ebfb3ac7b
Kirsty Ellis <kirsty.ellis@mila.quebec>
2025-07-11 15:05:26 -0400

    add image encoding

--- robo_gym/utils/camera.py
+++ robo_gym/utils/camera.py
@@ -10,6 +10,7 @@ class RoboGymCamera:
         self.image_mode = image_mode
         self.context_size = context_size
         self.num_cameras = num_cameras
+        self.image_encoding = ''
 
         self.observation_space = self.get_camera_observation_shape()
 
@@ -17,6 +18,7 @@ class RoboGymCamera:
         """Process camera images from string_params into proper format."""
 
         image_count = int(string_params.get("image_count", "0"))
+        self.image_encoding = string_params['image_encoding']
 
         if image_count == 0:
             if self.image_mode == 'temporal':
@@ -36,18 +38,33 @@ class RoboGymCamera:
                     nparr = np.frombuffer(image_bytes, np.uint8)
                     image = cv2.imdecode(nparr, cv2.IMREAD_COLOR)
 
-                    if image.shape != self.image_shape:
+                    if image.shape[:2] != self.image_shape[:2]:
                         image = cv2.resize(image, (self.image_shape[1], self.image_shape[0]))
 
-                    images.append(image)
+                    if self.image_encoding == 'yuv422_yuy2':
+
+                        yuv_image = cv2.cvtColor(image, cv2.COLOR_BGR2YUV)
+
+                        yuv422_image = self._convert_to_yuv422_yuy2(yuv_image)
+                        images.append(yuv422_image)
+                    else:
+                        images.append(image)
 
                 except Exception as e:
                     print(f"Warning: Failed to decode camera_image_{i}: {e}")
-                    images.append(np.zeros(self.image_shape, dtype=np.uint8))
+                    if self.image_encoding == 'yuv422_yuy2':
+                        zero_shape = (self.image_shape[0], self.image_shape[1], 2)
+                        images.append(np.zeros(zero_shape, dtype=np.uint8))
+                    else:
+                        images.append(np.zeros(self.image_shape, dtype=np.uint8))
 
         if self.image_mode == 'temporal':
             while len(images) < self.context_size:
-                images.insert(0, np.zeros(self.image_shape, dtype=np.uint8))
+                if self.image_encoding == 'yuv422_yuy2':
+                    zero_shape = (self.image_shape[0], self.image_shape[1], 2)
+                    images.insert(0, np.zeros(zero_shape, dtype=np.uint8))
+                else:
+                    images.insert(0, np.zeros(self.image_shape, dtype=np.uint8))
 
             images = images[-self.context_size:]
 
@@ -58,10 +75,26 @@ class RoboGymCamera:
             if images:
                 return images[-1].astype(np.uint8)
             else:
-                return np.zeros(self.image_shape, dtype=np.uint8)
+                if self.image_encoding == 'yuv422_yuy2':
+                    zero_shape = (self.image_shape[0], self.image_shape[1], 2)
+                    return np.zeros(zero_shape, dtype=np.uint8)
+                else:
+                    return np.zeros(self.image_shape, dtype=np.uint8)
 
         else:
             raise ValueError(f"Unsupported image_mode: {self.image_mode}")
+
+    def _convert_to_yuv422_yuy2(self, yuv_image):
+        """Convert YUV444 image to YUV422 YUY2 format."""
+        height, width, _ = yuv_image.shape
+
+        yuv422 = np.zeros((height, width, 2), dtype=np.uint8)
+
+        yuv422[:, :, 0] = yuv_image[:, :, 0]
+        yuv422[:, ::2, 1] = yuv_image[:, ::2, 1]
+        yuv422[:, 1::2, 1] = yuv_image[:, 1::2, 2]
+
+        return yuv422
 
     def get_camera_observation_shape(self):
         """Calculate camera observation shape based on configuration."""
